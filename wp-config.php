44a3c278cf583ae6f829d87ce8dd7d8fb7bc73fc
Johan Martin <martin.johan@nziswano.co.za>
2021-07-29 15:14:56 +0000

    Add multisite to wordpress

--- wp-config.php
+++ wp-config.php
@@ -84,14 +84,7 @@ $table_prefix  = 'wp_';
  */
 define('WP_DEBUG', getenv('WP_DEBUG'));
 /* Multisite */
-/* define('SUNRISE', 'on' ); */
-// define('WP_ALLOW_MULTISITE', true);
-// define('MULTISITE', true);
-// define('SUBDOMAIN_INSTALL', false);
-// define('DOMAIN_CURRENT_SITE', 'api.paseo.org.za');
-// define('PATH_CURRENT_SITE', '/');
-// define('SITE_ID_CURRENT_SITE', 1);
-// define('BLOG_ID_CURRENT_SITE', 1);
+define( 'WP_ALLOW_MULTISITE', true );
 
 // S3
 // define( 'S3_UPLOADS_BUCKET', '');
